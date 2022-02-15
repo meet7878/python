ab6c32a5b3132334627f25aa18681973bb0dc1c1
root <root@DESKTOP-VA0K6UF.localdomain>
2022-02-15 05:03:19 -0500

    [IMP] Python: Lambda file added

---
+++ lambda/lamdetails.py
@@ -0,0 +1,15 @@
+from lib2to3.pgen2.token import MINUS
+
+
+# MINUS =lambda x,y: x-y
+
+# print(MINUS(9,4))
+
+# def MINUS(x,y):
+#     return x-y
+
+# print(MINUS(9,4))
+
+a=[(1,14),(8,6),(5,23)]
+a.sort(key=lambda x:x[0])
+print(a)
