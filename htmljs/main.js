5c634d6fa9a6dd0877ed0fa63cee9279d1fa64f2
root <root@DESKTOP-VA0K6UF.localdomain>
2022-02-18 00:41:44 -0500

    [IMP] html_javascript: function,variable declaration

---
+++ htmljs/main.js
@@ -0,0 +1,16 @@
+// $('p').hide();
+
+// document.write("welcome to meet");
+// console.log("meet")
+// var name=10;
+
+// var x =50;   
+// console.log(x);
+//console.error("something wrong");
+// document.getElementById("main");
+
+// let length = 16;                               // Number
+// let lastName = "Johnson";                      // String
+// let x = {firstName:"John", lastName:"Doe"};    // Object
+
+let x = 16 + "Volvo";
